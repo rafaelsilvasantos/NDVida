cc0341ee549951bbe4ae06c24730a0b191d54dd9
rafaelsilvasantos <rafaelsilvasantos@gmail.com>
2016-11-11 15:30:04 -0200

    Add files via upload

---
+++ NDVida_Site/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NDVida_Site.Startup))]
+namespace NDVida_Site
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
