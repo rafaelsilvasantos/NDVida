5f4e7440b6bba252e88c236c4ec2030c210c3586
rafaelsilvasantos <rafaelsilvasantos@gmail.com>
2016-11-11 15:21:02 -0200

    Add files via upload

---
+++ NDVida_Site_Test/RegistrarController_UnitTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NDVida_Site.Controllers;
+
+namespace NDVida_Site_Test
+{
+    [TestClass]
+    public class RegistrarController_UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            RegistrarController c = new RegistrarController();
+
+            c.Index(new NDVida_Site.Models.RegistrarViewModel());
+        }
+    }
+}
